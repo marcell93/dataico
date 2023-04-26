0e54fc9363263810af5421b73f46ef61a269e377
Marcell Calero <marcell93c@gmail.com>
2023-04-26 00:37:08 -0300

    problem 1

--- src/problem1.clj
+++ src/problem1.clj
@@ -1,19 +1,16 @@
 (ns problem1)
 
+(defmacro xor
+  ([] nil)
+  ([a] a)
+  ([a b]
+   `(let [a# ~a
+          b# ~b]
+      (if a#
+        (if b# false true)
+        (if b# true false)))))
+
 (defn get-filtered-invoices [invoices]
-  (->> invoices
-       (filter
-        (fn [i]
-          (and
-           (and
-            (and
-             (i :taxable/taxes)
-             (->> some
-                  (i :taxable/taxes)
-                  (fn [t]
-                    (and (= (t :tax/category) :iva) (= (t :tax/rate) 19)))))
-            (i :retentionable/retentions))
-           (->> some
-                (i :retentionable/retentions)
-                (fn [r]
-                  (and (= (r :retention/category) :ret_fuente) (= (r :retention/rate) 1)))))))))
+  (defn invoice-filter [i] (xor (some (fn [t] (and (= (:tax/category t) :iva) (= (:tax/rate t) 19))) (:taxable/taxes i))
+                                (some (fn [r] (and (= (:retention/category r) :ret_fuente) (= (:retention/rate r) 1))) (:retentionable/retentions i))))
+  (->> invoices (filter invoice-filter)))
--- test/problem1.test.clj
+++ test/problem1.test.clj
@@ -7,11 +7,9 @@
 
 (def filtered-invoices (problem1/get-filtered-invoices (invoice :invoice/items)))
 
-(def filtered-id ((first filtered-invoices) :invoice-item/id))
-
-(def first-invoice-id ((first (invoice :invoice/items)) :invoice-item/id))
+(def filtered-ids (map #(:invoice-item/id %) filtered-invoices))
 
 (deftest problem1-test
-  (is (= first-invoice-id filtered-id)))
+  (is (= filtered-ids ["ii3" "ii4"])))
 
 (run-tests 'problem1.test)
